0b23f6df0065e2951541cafb4bcd76ff6072f3cf
Andressa Silva <developerandressa@gmail.com>
2020-09-25 22:44:37 -0300

    Implementação dos botões iniciais, descrição e valor gasto

--- lib/main.dart
+++ lib/main.dart
@@ -13,35 +13,63 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("TALKING ABOUT FINANCE"),
-        centerTitle: true,
-        backgroundColor: Colors.deepOrange,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text(),
-            Container(),
-            Container(),
-            Container(),
-            Container(),
-            Row(
-              children: [
-                Container(),
-                Container(),
-              ],
-            ),
-
+        appBar: AppBar(
+          title: Text("TALKING ABOUT FINANCE"),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
           ],
         ),
-      )
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 20.0),
+              Text('How much money did you spent today?'),
+              SizedBox(height: 20.0),
+              // Text Description and Button Value
+              Container(
+                width: 600.0,
+                height: 200.0,
+                color: Colors.amber,
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 200.0,
+                        height: 50.0,
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: RaisedButton(
+                          onPressed: (){},
+                          child: Text('Description'),
+                        ),
+                      ),
+                      SizedBox(width: 5.0),
+                      Container(
+                        width: 200.0,
+                        height: 50.0,
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          child: Text('Value'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+
+            ],
+          ),
+        ));
   }
 }
